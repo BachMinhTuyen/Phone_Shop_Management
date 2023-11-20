51d104c245876498cdec888254100758aa45e975
RanieStudyCode <xuanlan22102003@gmail.com>
2023-11-20 17:35:18 +0700

    hoàn thanh form

--- PhoneShopManagement/Program.cs
+++ PhoneShopManagement/Program.cs
@@ -20,14 +20,19 @@ namespace PhoneShopManagement
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new frmLogin());
             //Application.Run(new frmUserProfile());
-            // Application.Run(new frmProduct());
-            //Application.Run(new frmStaff());
+            // Application.Run(new frmProduct())
             //Application.Run(new frmAdmin("NV003"));
-           // Application.Run(new frmCustomer());
-            //Application.Run(new frmBrand());
-           // Application.Run(new frmUserAccount());
+            //Application.Run(new frmProduct());
+            //Application.Run(new frmStaff());
 
+            //Application.Run(new frmAdmin("NV003"));
+           //Application.Run(new frmCustomer());
+            //Application.Run(new frmBrand());
+           //Application.Run(new frmUserAccount());
            //Application.Run(new frmBill());
+
+            
+
         }
     }
 }
